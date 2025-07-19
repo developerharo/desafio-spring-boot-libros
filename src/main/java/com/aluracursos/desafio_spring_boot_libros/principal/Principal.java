bb1eb4ffa043555ca67a3a126ab705eceb87452b
developerharo <developerharo@gmail.com>
2025-07-18 22:27:54 -0600

    Creando la funcionalidad 'Top 10. Libros mas descargados'

--- src/main/java/com/aluracursos/desafio_spring_boot_libros/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio_spring_boot_libros/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio_spring_boot_libros.principal;
 
 import com.aluracursos.desafio_spring_boot_libros.model.Datos;
+import com.aluracursos.desafio_spring_boot_libros.model.DatosLibros;
 import com.aluracursos.desafio_spring_boot_libros.service.ConsumoAPI;
 import com.aluracursos.desafio_spring_boot_libros.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private ConsumoAPI consumoAPI = new ConsumoAPI();
@@ -13,5 +16,15 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+    // Top 10 libros mas descargados
+        System.out.println("Top 10 libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+
     }
 }
